07f9478103660f5b688d829e91f1df50750167a7
ms2138 <m38371@gmail.com>
2020-04-28 21:06:44 -0700

    Add tableView property to TodoViewController

--- Todo/View Controllers/TodoViewController.swift
+++ Todo/View Controllers/TodoViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class TodoViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 }
