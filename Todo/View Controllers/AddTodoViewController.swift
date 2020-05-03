396ef82c7fdb9a48e41844c1c3f9c35557e55cdf
ms2138 <m38371@gmail.com>
2020-05-02 19:38:19 -0700

    Add AddTodoViewController class

---
+++ Todo/View Controllers/AddTodoViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddTodoViewController.swift
+//  Todo
+//
+//  Created by mani on 2020-05-02.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
